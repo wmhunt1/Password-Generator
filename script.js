568262183de3902ce96bfebc31c51b2da3022beb
wmhunt1 <65505968+wmhunt1@users.noreply.github.com>
2020-07-10 12:04:41 -0400

    added number generation

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
 
 function generate_password()
 {
+    // prompts user for number of characters
     var input = prompt("Choose the number of characters")
     console.log(input)
+    // checks if it's a number
     var numchars = parseInt(input)
     console.log(numchars)
+    // lets you know if not a number
     if(isNaN(input))
     {
         alert ("Not a Number")
     }
     else
     {
+        //checks if in range
         if (numchars < 2 || numchars > 100)
         {
             alert ("Invalid input")
         }
         else
         {
-            alert ("password generated")
+            //generates password based on desired length
+            for (var i = 0; i < numchars; i++)
+            {
+                var digit = Math.floor((Math.random()*10))
+                console.log(digit)
+                // need to add letters and special characters with random for which one
+                // need to put them alltogether in a string and post it.
+            }
+            //alert ("password generated")
         }
     }
 }
