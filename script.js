176ab66006fb8d1cae4a74d54f33d8ba732db48b
wmhunt1 <65505968+wmhunt1@users.noreply.github.com>
2020-07-10 11:47:37 -0400

    works with prompt will fix form input later

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-var input = document.getElementById("userInput");
-console.log(input);
+
 function generate_password()
 {
-
+    var input = prompt("Choose the number of characters")
+    console.log(input)
+    var numchars = parseInt(input)
+    console.log(numchars)
 
 }
